7c7c0b516e9a245fd7e064dac0c415944f86f20a
Azalurg <patryk31415@gmail.com>
2022-04-27 10:57:08 +0200

    make it english

--- lab01/main.py
+++ lab01/main.py
@@ -1,21 +1,22 @@
 import sys
-from src import cezar
+from src import cezar, anarf
 s1, s2 = sys.argv[1], sys.argv[2]
 
 if s1 in ["-c", "c"]:
     code = cezar
 elif s1 in ["-a", "a"]:
-    print("anarf")
+    code = anarf
 else:
-    print("error")
+    print("Wrong parameter: {}".format(s1))
+    exit()
 
 if s2 in ["-e", "e"]:
-    print(code.szyfrowanie())
+    print(code.code_message())
 elif s2 in ["-d", "d"]:
-    print(code.odszyfrowanie())
+    print(code.read_code())
 elif s2 in ["-j", "j"]:
     print("krypto z jawnym")
 elif s2 in ["-k", "k"]:
     print("krypto bez jawnego")
 else:
-    print(error)
+    print("Wrong parameter: {}".format(s2))
