1a8c41ef6199c9cdf567f2b50802785f353d9b0a
Azalurg <patryk31415@gmail.com>
2022-04-28 19:11:15 +0200

    affine code start

--- lab01/src/anarf.py
+++ lab01/src/anarf.py
@@ -4,19 +4,24 @@ def code_message():
     with open("data/plain.txt", "r") as f:
         message = f.read()
         message = message.replace('\n', ' ')
-        message = message.lower()
 
     with open("data/key.txt", 'r') as f:
-        key = int(f.readline().split(" ")[0])
-        key = key % 26
+        key = f.readline().split(" ")
+        key[0], key[1] = int(key[0]), int(key[1])
+        if key[0] < 0 or key[1] < 0:
+            return "Wrong key: {}, {}".format(key[0], key[1])
+        key[0] = key[0] % 26
 
     for character in message:
-        if character != " ":
-            a = ord(character) + key
-            if a > 122:
+        a = ord(character)
+        if a in range(65, 91):
+            a = a*key[1] + key[0]
+            while a > 90:
                 a = a - 26
-        else:
-            a = 32
+        if a in range(97, 123):
+            a = a * key[1] + key[0]
+            while a > 122:
+                a = a - 26
         cryptogram += chr(a)
 
     with open("data/crypto.txt", "w") as f:
@@ -25,27 +30,86 @@ def code_message():
     return cryptogram
 
 
+# todo find a^ and finish code below
+
+
 def read_code():
     message = ""
     with open("data/crypto.txt", "r") as f:
         cryptogram = f.read()
         cryptogram = cryptogram.replace('\n', ' ')
-        cryptogram = cryptogram.lower()
 
     with open("data/key.txt", 'r') as f:
-        key = int(f.readline().split(" ")[0])
-        key = key % 26
+        key = f.readline().split(" ")
+        key[0], key[1] = int(key[0]), int(key[1])
+        if key[0] < 0 or key[1] < 0:
+            return "Wrong key: {}, {}".format(key[0], key[1])
+        key[0] = key[0] % 26
 
     for character in cryptogram:
-        if character != " ":
-            a = ord(character) - key
+        a = ord(character)
+        if a in range(65, 91):
+            a = a + key[0]
+            while a > 90:
+                a = a - 26
+        if a in range(97, 123):
+            a -= key
             if a < 97:
                 a = a + 26
-        else:
-            a = 32
         message += chr(a)
 
     with open("data/decrypt.txt", "w") as f:
         f.write(message)
 
     return message
+
+
+def find_key():
+    with open("data/crypto.txt", "r") as f:
+        cryptogram = f.read()
+        cryptogram = cryptogram.replace('\n', ' ')
+
+    with open("data/extra.txt", "r") as f:
+        extra = f.read()
+        extra = extra.replace('\n', ' ')
+        extra = extra.split(" ")
+
+    for index_word, word in enumerate(cryptogram):
+        for index_character, character in enumerate(word):
+            if ord(character) in range(65, 91) or ord(character) in range(97, 123):
+                key = (ord(character) - ord(extra[index_word][index_character])) % 26
+                with open("data/key-found.txt", "w") as f:
+                    f.write(str(key))
+                return key
+
+    key = "Can't find a key :-("
+
+    with open("data/key-found.txt", "w") as f:
+        f.write(str(key))
+
+    return key
+
+
+def break_code():
+    message = ""
+    with open("data/crypto.txt", "r") as f:
+        cryptogram = f.read()
+        cryptogram = cryptogram.replace('\n', ' ')
+    with open("data/decrypt.txt", "w") as f:
+        for key in range(1, 26):
+            for character in cryptogram:
+                a = ord(character)
+                if a in range(65, 91):
+                    a -= key
+                    if a < 65:
+                        a = a + 26
+                if a in range(97, 123):
+                    a -= key
+                    if a < 97:
+                        a = a + 26
+                message += chr(a)
+            message += "\n"
+            f.write(message)
+            message = ""
+
+    return "Done"
