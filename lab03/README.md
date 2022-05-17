3ce12945db56c34ac229ad29bb5cfb88395b2938
Azalurg <patryk31415@gmail.com>
2022-05-17 14:11:33 +0200

    the end

---
+++ lab03/README.md
@@ -0,0 +1,13 @@
+# lab06
+
+<table>
+    <tr>
+        <td><img src="./images/plain.bmp" alt="Can't find photo" width=100%></td>
+    </tr>
+  <tr>
+    <td><img src="./images/ecb_crypto.bmp" alt="Can't find photo" width=100%></td>
+  </tr>
+    <tr>
+    <td><img src="./images/cbc_crypto.bmp" alt="Can't find photo" width=100%></td>
+  </tr>
+</table>
--- lab03/main.py
+++ lab03/main.py
@@ -2,15 +2,27 @@ from PIL import Image
 import hashlib
 import random
 
+input_image = Image.open("images/plain.bmp")
+
+
+def save(data, where="images/image.bmp", message="Done"):
+    new_data = bytes(data)
+
+    output_image = input_image.copy()
+    output_image.frombytes(new_data)
+    output_image.save(where)
+    print(message)
+
+
 block_size = 8
-input_image = Image.open("images/mcqueen.bmp")
 image_data = input_image.tobytes()
-new_data = []
 size = input_image.size
 
+# ECB
+new_data = []
 keys = []
 for x in range(block_size):
-    #key = hashlib.md5(str(x ** 3 + x).encode("UTF-8")).digest()
+    # key = hashlib.md5(str(x ** 3 + x).encode("UTF-8")).digest()
     key = hashlib.md5(str(random.random() * x).encode("UTF-8")).digest()
     keys.append(key)
 
@@ -21,9 +33,15 @@ for x in range(size[0]):
         pta = op ^ keys[x % block_size][y % block_size]     # pixel to add
         new_data.append(pta)
 
-new_data = bytes(new_data)
+save(new_data, "images/ecb_crypto.bmp", "you can see me :-(")
 
-output_image = input_image.copy()
-output_image.frombytes(new_data)
-output_image.save("images/ECB.bmp")
-print("I'm speed!")
+
+# CBC
+new_key = 13
+new_data = [image_data[0] ^ new_key]
+for x in range(size[0]*size[1]):
+    new_data.append(new_data[x-1] ^ image_data[x] ^ keys[x%64//8][x%8])
+
+save(new_data, "images/cbc_crypto.bmp", "you can't see me ;-D")
+
+print("Done")
